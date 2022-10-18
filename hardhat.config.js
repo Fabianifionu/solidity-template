a704663e91e24b9a89312831bb549dd80a4206df
Fabianifionu <113173081+Fabianifionu@users.noreply.github.com>
2022-10-18 15:26:34 +0200

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require('@nomiclabs/hardhat-etherscan');
+require('@nomiclabs/hardhat-truffle5');
+require('dotenv').config();
+require('hardhat-deploy');
+require('hardhat-gas-reporter');
+require('solidity-coverage');
+
+const { networks, etherscan } = require('./hardhat.networks');
+
+module.exports = {
+    etherscan,
+    networks,
+    solidity: {
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000000,
+            },
+        },
+        version: '0.8.15',
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+};
